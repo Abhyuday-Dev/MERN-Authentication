8a10d7f0569bf11069102d3ca5310efc482ad591
Abhyuday-Dev <abhyuday7176@hotmail.com>
2023-07-24 20:45:41 +0530

    login and signup controllers

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose');
 const bcrypt=require('bcrypt');
 const validator=require('validator');
 
+
 const Schema=mongoose.Schema
 
 const userSchema=new Schema({
@@ -44,6 +45,27 @@ userSchema.statics.signup = async function(email,password){
     const user=await this.create({ email,password:hash });
 
     return user;
+}
+
+//static login method
+userSchema.statics.login=async function(emal,password){
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+
+    const user=await this.findOne({ email})
+
+    if(!user){
+        throw Error('Incorrect Email'); 
+    }
+
+    const match=await bcrypt.compare(password,user.password)
+    if(!match){
+        throw Error("Incorrect password")
+    }
+
+    return user;
+
 }
 
 module.exports=mongoose.model('User',userSchema);
